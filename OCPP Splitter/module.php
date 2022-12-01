b702e9a31fd8879beadf5e463d1b4c1319c33a6a
Michael Maroszek <maroszek@gmx.net>
2022-12-01 10:44:18 +0100

    add better splitter configuration form

--- OCPP Splitter/module.php
+++ OCPP Splitter/module.php
@@ -17,7 +17,6 @@ include_once __DIR__ . '/../libs/WebHookModule.php';
         {
             //Never delete this line!
             parent::Create();
-            $this->RegisterPropertyString('Address', 'ws://127.0.0.1:3777/hook/ocpp/' . $this->InstanceID);
         }
 
         public function Destroy()
@@ -28,20 +27,17 @@ include_once __DIR__ . '/../libs/WebHookModule.php';
 
         public function ForwardData($data)
         {
-            $data = json_decode($data);
-            $buffer = json_encode($data->Buffer);
-            $this->SendDebug('Send Data', print_r($buffer, true), 0);
-
-            $this->send($buffer);
+            $this->send(json_decode($data)->Buffer);
         }
 
         protected function ProcessHookData()
         {
-            //Get the input
-            $input = json_decode(file_get_contents('php://input'));
-            $this->SendDebug('Data', json_encode($input), 0);
+            $data = file_get_contents('php://input');
+            $this->SendDebug('Receive', $data, 0);
+            $data = json_decode($data);
+            
             //Send it to the children
-            $this->SendDataToChildren(json_encode(['DataID'=> '{54E04042-D715-71A0-BA80-ADD8B6CDF151}', 'Buffer' => $input]));
+            $this->SendDataToChildren(json_encode(['DataID'=> '{54E04042-D715-71A0-BA80-ADD8B6CDF151}', 'Buffer' => $data]));
 
             /**
              * OCPP-j-1.6-specification Page 13
@@ -50,23 +46,34 @@ include_once __DIR__ . '/../libs/WebHookModule.php';
              *
              * Switch because there can be more MessageTypes
              */
-            switch ($input[2]) {
+            switch ($data[2]) {
                 case 'BootNotification':
-                    $this->SendDebug('Hi', 'hshg', 0);
-                    $package = $this->getBootNotificationResponse($input[1]);
+                    $this->send($this->getBootNotificationResponse($data[1]));
                     break;
 
                 default:
-                    $package = '';
                     break;
             }
-
-            $this->send($package);
         }
 
+        public function GetConfigurationForm() {
+            $form = json_decode(file_get_contents(__DIR__ . '/form.json'), true);
+            
+            $interfaces = Sys_GetNetworkInfo();
+            $ipList = [];
+            foreach ($interfaces as $interface) {
+                $ipList[] = $interface['IP'];
+            }
+            
+            $form['actions'][2]['caption'] = sprintf($this->Translate($form['actions'][2]['caption']), implode(', ', $ipList));
+            $form['actions'][4]['caption'] = sprintf($this->Translate($form['actions'][4]['caption']), 'hook/ocpp/' . $this->InstanceID);
+            return json_encode($form);
+        }
+        
         private function send($package)
         {
             $package = json_encode($package);
+            $this->SendDebug('Transmit', $package, 0);
             WC_PushMessage(IPS_GetInstanceListByModuleID('{015A6EB8-D6E5-4B93-B496-0D3F77AE9FE1}')[0], '/hook/ocpp/' . $this->InstanceID, $package);
         }
 
